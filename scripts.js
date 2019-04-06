e85dcd4c1a743b320221cab9ed71b359ee1846a7
Cody Williamson <codywscw@gmail.com>
2019-04-06 09:41:45 -0500

    hero fixed on mobile, close nav on link click

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-$('.nav__trigger').on('click', function(e){
+$('.nav__trigger',).on('click', function(e){
      e.preventDefault();
-     $(this).parent().toggleClass('nav--active');
+     $(this).parent().toggleClass('nav--active').show();
    });
+
+$('.nav li').on('click', function(){
+  $(".nav__trigger").parent().removeClass("nav--active");
+  $(".nav__icon").removeClass("nav--active");
+});
