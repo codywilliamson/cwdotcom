fabcc620be85abb24750297e272c906153a6e49d
Cody Williamson <codywscw@gmail.com>
2019-04-11 10:50:25 -0500

    navigation on desktop, about tweaks

--- scripts.js
+++ scripts.js
@@ -7,3 +7,17 @@ $('.nav li').on('click', function(){
   $(".nav__trigger").parent().removeClass("nav--active");
   $(".nav__icon").removeClass("nav--active");
 });
+
+var myNav = document.querySelector("nav");
+var nav = document.querySelector(".nav__list");
+
+window.onscroll = function() {
+  "use strict";
+  if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+    nav.classList.add("scrolled");
+    myNav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+    myNav.classList.remove("scrolled");
+  }
+};
