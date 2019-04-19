62a354102bf9ab41bc0f2a99f2b445e77f21667c
Cody Williamson <codywscw@gmail.com>
2019-04-18 23:59:12 -0500

    favicons update

--- scripts.js
+++ scripts.js
@@ -1,32 +1,3 @@
-/*
-  smooth scroll from w3schools
-  link: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
-*/
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-
 // full screen mobile menu
 $('.nav__trigger',).on('click', function(e){
      e.preventDefault(); // prevent default behavior
@@ -54,3 +25,8 @@ window.onscroll = function() {
     myNav.classList.remove("scrolled");
   }
 };
+
+/*
+  minified smooth scroll from w3schools
+*/
+$(document).ready(function(){$("a").on("click",function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},800,function(){window.location.hash=t})}})});
