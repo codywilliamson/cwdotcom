81f0d725e641a2fc67ba8da6d169b8b33d480d70
codywilliamson <codywscw@gmail.com>
2019-10-31 23:33:56 -0500

    add countdown

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -18,9 +18,11 @@ var timer = document.querySelector(".timer");
     var hours = Math.floor(distance % ( 1000 * 60 * 60 * 24 ) / ( 1000 * 60 * 60 ));
     var minutes = Math.floor(distance % ( 1000 * 60 * 60 ) / ( 1000 * 60 ));
     var seconds = Math.floor(distance % ( 1000 * 60 ) / 1000);
+    var weeks = Math.floor(days / 7);
 
     // display the result
     timer.innerHTML = days + "d : " + hours + "h : " + minutes + "m : " + seconds + "s";
+    document.querySelector(".weeks").innerHTML = weeks + " weeks";
 
     // if time is finished, display finished message
     if (distance < 0) {
