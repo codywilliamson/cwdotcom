6921c85fc404521fc49181081fe321bc0001f447
Cody <codywscw@gmail.com>
2024-03-08 02:38:06 -0600

    clean before PR to master

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -16,7 +16,7 @@ const presentation: Presentation = {
   title: "Hey, I'm Cody.",
   // profile: "/profile.webp",
   description:
-    "I'm *husband, father, and software engineer*, and a few more choice words. Here I'll showcase my work, experience, and pretend like I might keep up with the blogging thing. Currently, I work for *OneStream Software* in a Cloud Developer role during the day, and at night, I run *Spectacle Web Design*. Anything else?",
+    "I'm a *husband, father, and software engineer*, and a few more choice words. Here I'll showcase my work, experience, and pretend like I might keep up with the blogging thing. Currently, I work for *OneStream Software* in a *Cloud Developer* role during the day, and at night, I run *Spectacle Web Design*. Anything else?",
   socials: [
     {
       label: "Github",
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,37 +6,37 @@ export type Project = {
 };
 
 const projects: Project[] = [
-  {
-    title: "Auburn Vetmag",
-    techs: ["WordPress"],
-    link: "https://mag.auburnvetmed.com/",
-  },
-  {
-    title: "Isbell Lawn Care",
-    techs: ["11ty", "Spectacle Web Design"],
-    link: "https://isbelllawncare.com/",
-  },
-  {
-    title: "MPH Detailing",
-    techs: ["11ty", "Spectacle Web Design"],
-    link: "https://mphdetailing.com/",
-  },
+  // {
+  //   title: "Auburn Vetmag",
+  //   techs: ["WordPress"],
+  //   link: "https://mag.auburnvetmed.com/",
+  // },
+  // {
+  //   title: "Isbell Lawn Care",
+  //   techs: ["11ty", "Spectacle Web Design"],
+  //   link: "https://isbelllawncare.com/",
+  // },
+  // {
+  //   title: "MPH Detailing",
+  //   techs: ["11ty", "Spectacle Web Design"],
+  //   link: "https://mphdetailing.com/",
+  // },
   {
     title: "Spectacle Web Design",
     techs: ["11ty", "Spectacle Web Design"],
     link: "https://spectaclewebdesign.com/",
   },
-  {
-    title: "John Ed Mathison",
-    techs: ["WordPress"],
-    link: "https://johnedmathison.org",
-  },
-  {
-    title: "ScottHunts.com",
-    techs: ["WordPress", "Community Building"],
-    link: "/",
-    isComingSoon: true
-  },
+  // {
+  //   title: "John Ed Mathison",
+  //   techs: ["WordPress"],
+  //   link: "https://johnedmathison.org",
+  // },
+  // {
+  //   title: "ScottHunts.com",
+  //   techs: ["WordPress", "Community Building"],
+  //   link: "/",
+  //   isComingSoon: true
+  // },
   {
     title: "Spectacle Software Rebrand",
     techs: ["Astro", "Vue"],
