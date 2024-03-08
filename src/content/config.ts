abf72dd65be1e6405aedb6bc74ddb2209c53b5d5
Cody <codywscw@gmail.com>
2024-03-08 02:29:01 -0600

    blog template

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+    description: z.string(),
+    isPublish: z.boolean(),
+    isDraft: z.boolean().default(false),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { posts: postsCollection };
---
+++ src/data/projects.ts
@@ -0,0 +1,54 @@
+export type Project = {
+  title: string;
+  techs: string[];
+  link: string;
+  isComingSoon?: boolean;
+};
+
+const projects: Project[] = [
+  {
+    title: "Auburn Vetmag",
+    techs: ["WordPress"],
+    link: "https://mag.auburnvetmed.com/",
+  },
+  {
+    title: "Isbell Lawn Care",
+    techs: ["11ty", "Spectacle Web Design"],
+    link: "https://isbelllawncare.com/",
+  },
+  {
+    title: "MPH Detailing",
+    techs: ["11ty", "Spectacle Web Design"],
+    link: "https://mphdetailing.com/",
+  },
+  {
+    title: "Spectacle Web Design",
+    techs: ["11ty", "Spectacle Web Design"],
+    link: "https://spectaclewebdesign.com/",
+  },
+  {
+    title: "John Ed Mathison",
+    techs: ["WordPress"],
+    link: "https://johnedmathison.org",
+  },
+  {
+    title: "ScottHunts.com",
+    techs: ["WordPress", "Community Building"],
+    link: "/",
+    isComingSoon: true
+  },
+  {
+    title: "Spectacle Software Rebrand",
+    techs: ["Astro", "Vue"],
+    link: "/",
+    isComingSoon: true
+  },
+  {
+    title: "... and so many others not publically available",
+    techs: ["C#", "Azure", "Angular", "Python", "PowerShell"],
+    link: "/",
+    isComingSoon: true
+  },
+];
+
+export default projects;
