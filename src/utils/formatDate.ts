f3da44570688beaa0d155a7631b64a17e9d5a821
Cody <codywscw@gmail.com>
2024-03-19 16:20:10 -0500

    astro test

--- src/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export default function formatDate(date: Date) {
-  return new Intl.DateTimeFormat('en-US', { 
-    month: '2-digit', 
-    day: '2-digit', 
-    year: 'numeric' 
-  }).format(date);
-}
